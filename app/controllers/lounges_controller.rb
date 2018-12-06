721935821d2ed6b0d2a5c5d2e1cb4b4515db6f8b
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2018-12-06 16:36:10 +0100

    now the bookings are shown properly

--- app/controllers/lounges_controller.rb
+++ app/controllers/lounges_controller.rb
@@ -11,12 +11,42 @@ class LoungesController < ApplicationController
     end
   end
 
+  #TODO - THIS CONTROLLER IS TOO FAT
   def show
     id = params[:id]
     @lounge = Lounge.find(id)
     @booking = Booking.new
+    bookings = Booking.where(lounge_id: id)
     if @lounge[:is_legacy] == true
       @availability = Availability.where(lounge_id: id)
+      bookings.each do |booking|
+        @availability.map do |avai| 
+          if avai[:date] == booking.date
+            avai[:remaining] -= booking.total_people
+          else
+            avai
+          end
+        end
+      end
+    else
+      @availability = []
+      for item in (0...20) do
+        avai_hash = {
+          date: Date.today + item,
+          remaining: @lounge.capacity
+        }
+        @availability.push(avai_hash)
+      end
+
+      bookings.each do |booking|
+        @availability.map do |avai| 
+          if avai[:date] == booking.date
+            avai[:remaining] -= booking.total_people
+          else
+            avai
+          end
+        end
+      end
     end
   end
 
